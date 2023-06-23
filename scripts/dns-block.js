eafa433a786af35e8d712fde9165e0a6ce159974
Liam Rahum <liam@rahum.co.il>
2023-06-23 13:44:18 +0300

    Build: reverted to Manifest v2

--- scripts/dns-block.js
+++ scripts/dns-block.js
@@ -1,36 +1,27 @@
-var responseVerses = {
-    "lust" : 
-    [
-        {
-        "verse": "Matthew 5:28", 
-        "isNT": true
-        },
-        {
-            "verse": "Proverbs 6:25-29", 
-            "isNT": false
-        },
-        {
-            "verse": "Romans 1:21-27", 
-            "isNT": true
-        },
-        {
-            "verse": "2 Timothy 2:22", 
-            "isNT": true
-        }
-    ]
+const blockedWebsites = [
+  'example.com',
+  'blockedwebsite.com',
+];
+
+// Function to block and redirect websites
+function blockAndRedirect(requestDetails) {
+  console.log("Something");
+   const url = new URL(requestDetails.url);
+  const hostname = url.hostname;
+
+  // Check if the requested website is in the blocked list
+  if (blockedWebsites.includes(hostname)) {
+    // Redirect the user to a different webpage
+    return { redirectUrl: 'https://example.com/blocked.html' };
+  }
+
+  // Allow the request to proceed
+  return { cancel: false };
 }
 
+chrome.webRequest.onBeforeRequest.addListener(
+  blockAndRedirect,
+  { urls: ['<all_urls>'] },
+  ['blocking']
+);
 
-findURL = function changeURL(text, category)
-{
-    var current = window.location.href;
-    var response = responseVerses[category][Math.floor(Math.random() * responseVerses[category].length)];
-    var isNT = response["isNT"]
-
-    if(current.indexOf(text) !== -1)
-        window.location.replace("https://www.biblegateway.com/passage/?search=" + response["verse"] + "&version=" + (isNT ? "HHH" : "NIV"));
-   
-}
-
-
-findURL("example.com", "lust");
